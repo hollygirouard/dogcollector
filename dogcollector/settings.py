f58e4386c3be9bc8a48a8042e5eefc9fef8df521
holly <hollyelizabethwhite@gmail.com>
2021-09-13 08:56:10 -0600

    working push

--- dogcollector/settings.py
+++ dogcollector/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'dogcollector.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'catcollector',
+        'NAME': 'dogcollector',
     }
 }
 
